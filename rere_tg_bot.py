dce56f6f26bf57462fb26732d4f7c6a71c7a9e74
txmxmx <rere.txmxmx.sp@gmail.com>
2022-04-05 22:07:22 +0400

    c04

--- rere_tg_bot.py
+++ rere_tg_bot.py
@@ -1,34 +1,34 @@
 import logging
-from aiogram import Bot, Dispatcher, executor, types
+from aiogram import Bot, Dispatcher, executor, types, filters
 from config import TOKEN, WEATHER_TOKEN
 from aiogram.dispatcher.filters import Text
-
 import requests
 import datetime
-
+import asyncio
+ 
 bot = Bot(token = TOKEN)
 dp = Dispatcher(bot)
 
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level = logging.INFO)
 
 # Create mainKeyboard
 @dp.message_handler(commands = "start")
-async def cmd_start(message: types.Message):
+async def main_menu(message: types.Message):
     mainKeyboard = types.ReplyKeyboardMarkup(resize_keyboard = True)
     # Create mainButtons
-    mainButtons = ["My repository", "Contacts", "Weather"]
+    mainButtons = ["My repository", "Contacts", "Show me pussy", "Weather"]
     mainKeyboard.add(*mainButtons)
     # Main Message
     await message.answer("Hello, my name is RERE\nI'm bot for telegram\nI programmed to transmit information", reply_markup = mainKeyboard)
-   
+
 # "My repository"'s button response
 @dp.message_handler(lambda message: message.text == "My repository")
-async def without_puree(message: types.Message):
+async def my_repository(message: types.Message):
     await message.reply("https://github.com/txmxmx/rere_tg_bot.git")
 
 # "Contacts"'s button response
 @dp.message_handler(lambda message: message.text == "Contacts")
-async def cmd_inline_url(message: types.Message):
+async def contacts(message: types.Message):
     # Create contactsButtons
     contactsButtons = [
         types.InlineKeyboardButton(text="Site", url= "http://txmxmx.ru/"),
@@ -40,16 +40,45 @@ async def cmd_inline_url(message: types.Message):
     # Message
     await message.answer("Links to contacts", reply_markup = contactsKeyboard)
 
+# "Show me pussy"'s button response
+@dp.message_handler(lambda message: message.text == "Show me pussy")
+async def show_pussy(message: types.Message):
+    catsKeyboard = types.ReplyKeyboardMarkup(resize_keyboard = True)
+    catsButtons = ["Yes", "No"]
+    catsKeyboard.add(*catsButtons)
+    await message.answer("Do you want to see my pussy?", reply_markup = catsKeyboard)
+
+# Answer YES
+@dp.message_handler(Text(equals = "Yes"))
+async def answer_yes(message: types.Message):
+    await message.reply("Okay, look")
+    await asyncio.sleep(1.5)
+    await types.ChatActions.upload_photo()
+    media = types.MediaGroup()
+    media.attach_photo(types.InputFile('data/img/answer_yes.jpg'))
+    await message.reply_media_group(media = media)
+
+# Answer No
+@dp.message_handler(Text(equals = "No"))
+async def answer_no(message: types.Message):
+    await message.reply("Mmm..")
+    await asyncio.sleep(1.5)
+    await types.ChatActions.upload_photo()
+    media = types.MediaGroup()
+    media.attach_photo(types.InputFile('data/img/answer_no.jpg'))
+    await message.reply_media_group(media = media)
+
 # "Weather"'s button response
 @dp.message_handler(lambda message: message.text == "Weather")
-async def start_command(message: types.Message):
-    await message.reply("Okay, Enter city name")
+async def weather(message: types.Message):
+    await message.reply("Okay, enter city name")
 
 # Logics
 @dp.message_handler()
 async def get_weather(message: types.Message):
-    # Smiles's codes
-    code_to_smile = {
+
+    # Code to smile
+    codeToSmile = {
         "Clear": "Clear \U0001F917",
         "Clouds": "Clouds \U0001F641",
         "Rain": "Rain \U0001F927",
@@ -59,40 +88,40 @@ async def get_weather(message: types.Message):
         "Mist": "Mist \U0001F47B"
     }
     
-    # Create requests
+    # Create logic
     try:
-        requests_to_weather = requests.get(
-            f"http://api.openweathermap.org/data/2.5/weather?q={message.text}&appid={WEATHER_TOKEN}&units=metric")
-        data = requests_to_weather.json()
+        requestsToWeather = requests.get(f"http://api.openweathermap.org/data/2.5/weather?q={message.text}&appid={WEATHER_TOKEN}&units=metric")
+        data = requestsToWeather.json()
 
         city = data["name"]
         cur_weather = data["main"]["temp"]
 
-        weather_description = data["weather"][0]["main"]
-        if weather_description in code_to_smile:
-            wd = code_to_smile[weather_description]
+        weatherDescription = data["weather"][0]["main"]
+
+        if weatherDescription in codeToSmile:
+            weatherData = codeToSmile[weatherDescription]
         else:
-            wd = "Look out the window, I do not understand what the weather is like there!"
+            weatherData = "Look out the window, I do not understand what the weather is like there!"
 
         # Search answer
         humidity = data["main"]["humidity"]
         pressure = data["main"]["pressure"]
         wind = data["wind"]["speed"]
-        sunrise_timestamp = datetime.datetime.fromtimestamp(data["sys"]["sunrise"])
-        sunset_timestamp = datetime.datetime.fromtimestamp(data["sys"]["sunset"])
-        length_of_the_day = datetime.datetime.fromtimestamp(data["sys"]["sunset"]) - datetime.datetime.fromtimestamp(data["sys"]["sunrise"])
+        sunriseTimestamp = datetime.datetime.fromtimestamp(data["sys"]["sunrise"])
+        sunsetTimestamp = datetime.datetime.fromtimestamp(data["sys"]["sunset"])
+        lengthOfTheDay = datetime.datetime.fromtimestamp(data["sys"]["sunset"]) - datetime.datetime.fromtimestamp(data["sys"]["sunrise"])
 
         # Create answer
         await message.reply(
             f"***{datetime.datetime.now().strftime('%Y-%m-%d %H:%M')}***\n"
-            f"City weather: {city}\nTemperature: {cur_weather}C° {wd}\n"
+            f"City weather: {city}\nTemperature: {cur_weather}C° {weatherData}\n"
             f"Humidity: {humidity}%\nPressure: {pressure}\nWind: {wind} m\s\n"
-            f"Sunrise: {sunrise_timestamp}\nSunset: {sunset_timestamp}\nDay length: {length_of_the_day}\n"
+            f"Sunrise: {sunriseTimestamp}\nSunset: {sunsetTimestamp}\nDay length: {lengthOfTheDay}\n"
             f"***Have a good day!***"
             )
 
     except:
-        await message.reply("\U0001F914 Check city name \U0001F615")       
+        await message.reply("\U0001F914 Check city name \U0001F615")   
    
 # Run bot
 if __name__ == "__main__":
